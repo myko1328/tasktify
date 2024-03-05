f7d81901d758dd0f24d9cf5a4e420f89beeb8836
mykomiparanum <m.miparanum@gmail.com>
2024-03-05 12:11:02 +0100

    create board UI

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,18 @@
-const Organizationpage = () => {
-  return <div>Organizationpage</div>;
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+import { Suspense } from "react";
+
+const Organizationpage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <Suspense fallback={<BoardList.Skeleton />}>
+        <BoardList />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Organizationpage;
